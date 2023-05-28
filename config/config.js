3b889ff17f16764b916ca8a2ef41f9d39d028442
Shivam Tewari <shivamtewari2002@gmail.cm>
2023-05-28 13:07:31 +0530

    pushing

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const Port = process.env.NODE_ENV === 'test' ? 4001 : (process.env.PORT || 4000);
 
-const MongodbUrl =  process.env.NODE_ENV === 'production' ? process.env.MONGODB_URL_PRODUCTION : (process.env.MONGODB_URL_DEVELOPMENT || 'mongodb://localhost/Aasara')
+const MongodbUrl =  process.env.NODE_ENV === 'production' ? process.env.MONGODB_URL_PRODUCTION : (process.env.MONGODB_URL_DEVELOPMENT || 'mongodb+srv://shivamtewari2002:nitkwifiworst@cluster0.3wzmcws.mongodb.net/?retryWrites=true&w=majority')
 
 module.exports = {
   Port: Port,
